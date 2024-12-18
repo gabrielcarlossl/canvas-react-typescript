38a1980028d6b457a6a86eb98bd6436df1889ea2
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2024-12-18 07:11:34 -0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Canvas from './components/Canvas'
+
+function App() {
+
+  return (
+    <>
+      <h1>Canva</h1>
+      <Canvas />
+    </>
+  )
+}
+
+export default App
